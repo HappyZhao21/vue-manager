f3dc840d6c5fa26b0562275a7c4e1c77a1e8ab1b
zxq <2671883469@qq.com>
2022-03-17 11:30:57 +0800

    1、axios封装

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/less/index.less'
+import router from "../router";
 import store from '../store'
 
-import router from "../router";
+import http from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+
+Vue.prototype.$http = http
+
+
 
 
 new Vue({
