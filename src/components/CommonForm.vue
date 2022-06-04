db2b687ca5d843b496dc63fbec417f6930923091
zxq <2671883469@qq.com>
2022-06-04 12:16:03 +0800

    优化公共表单组件、解决点击表单组件后不能选择其他菜单问题

--- src/components/CommonForm.vue
+++ src/components/CommonForm.vue
@@ -42,7 +42,7 @@ export default{
         inline:Boolean
     },
     data(){
-
+       return {}
     }
 }
 </script>
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,15 +1,173 @@
 <template>
-    <div>
-      hello this is user pages !!1
+    <div class="manage">
+      <el-dialog
+          :title="operateType === 'add' ? '新增用户' : '更新用户'"
+          :visible.sync="isShow"
+      >
+        <common-form
+            :formLabel="operateFormLabel"
+            :form="operateForm"
+            :inline="true"
+            ref="form"
+        ></common-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="isShow = false">取消</el-button>
+          <el-button type="primary" @click="confirm">确定</el-button>
+        </div>
+      </el-dialog>
+      <div class="manage-header">
+        <el-button type="primary" @click="addUser">+新增</el-button>
+
+        <common-form
+            :formLabel="formLabel"
+            :form="searchFrom"
+            :inline="true"
+            ref="form">
+          <el-button type="primary " @click="getList(searchFrom.keyword)">搜索</el-button>
+        </common-form>
+
+      </div>
+
     </div>
 </template>
 
 <script>
+
+import CommonForm from "@/components/CommonForm";
+
 export default {
-  name: "user"
+  name: "User",
+  components: {
+    CommonForm
+  },
+  data(){
+      return {
+        operateType: "add",
+        isShow: false,
+        operateFormLabel: [
+          {
+            model: "name",
+            label: "姓名",
+            type: "input",
+          },
+          {
+            model: "age",
+            label: "年龄",
+            type: "input",
+          },
+          {
+            model: "sex",
+            label: "性别",
+            type: "select",
+            opts: [
+              {
+                label: "男",
+                value: 1,
+              },
+              {
+                label: "女",
+                value: 0,
+              },
+            ],
+          },
+          {
+            model: "birth",
+            label: "出生日期",
+            type: "date",
+          },
+          {
+            model: "addr",
+            label: "地址",
+            type: "input",
+          },
+        ],
+        operateForm: {
+          name: "",
+          addr: "",
+          age: "",
+          birth: "",
+          sex: "",
+        },
+        formLabel: [
+          {
+            model: "keyword",
+            label: "",
+            type: "input",
+          },
+        ],
+        searchFrom: {
+          keyword: "",
+        },
+        tableData:[],
+        tableLabel:[
+          {
+            prop:"name",
+            label:"姓名"
+          },
+          {
+            prop:"age",
+            label:"年龄"
+          },
+          {
+            prop:"sexLabel",
+            label:"性别"
+          },
+          {
+            prop:"birth",
+            label:"出生日期",
+            width:200
+          },
+          {
+            prop:"addr",
+            label:"地址",
+            width:320
+          }
+        ],
+        config:{
+          page:1,
+          total:30
+        }
+      }
+  },
+  methods:{
+    confirm(){
+      if(this.operateType === 'edit'){
+        this.$http.post('user/edit',this.operateForm).then(res=>{
+          console.log(res)
+          this.isShow=false
+          this.getList()
+        })
+      }else{
+        this.$http.post('user/add',this.operateForm).then(res=>{
+          console.log(res)
+          this.isShow=false
+          this.getList()
+        })
+      }
+    },
+    addUser(){
+      (this.isShow = true),
+      (this.operateType = "add"),
+      (this.operateForm = {
+        name: "",
+        addr: "",
+        age: "",
+        birth: "",
+        sex: "",
+      });
+
+    },
+    getList(){
+
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+  .manage-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
